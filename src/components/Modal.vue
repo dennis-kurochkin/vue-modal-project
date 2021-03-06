1e5c2bbbcf1a9208c47a513329a66b7af342fe14
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2021-03-06 21:16:00 +0300

    fix(modal): closing

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="backdrop" @click="closeModal">
+  <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{ sale: theme === 'sale' }">
       <h1>{{ title }}</h1>
       <p>{{ content }}</p>
